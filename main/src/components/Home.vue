2c47c1c12cffa23369e3063870a9c608625a36c3
Jenny Le <jennyledangg@gmail.com>
2021-10-13 16:49:47 -0500

    Setting up spicy water

---
+++ main/src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <p>
+      Welcome to Spicy Water. An online way to interact and play with
+      friends online!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
